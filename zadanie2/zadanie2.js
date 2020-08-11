44f6b09c2dca64229c69ed7888654880fcdf35c1
ytokarevskaya <lan2214@yandex.ru>
2020-08-11 14:08:30 +0300

    Проверено

--- zadanie2/zadanie2.js
+++ zadanie2/zadanie2.js
@@ -2,4 +2,8 @@ const btn = document.querySelector('.j-btn-test');
 
 btn.addEventListener('click', () => {
     alert(`Размер экрана: ${window.innerWidth} x ${window.innerHeight}`)
-})
+});
+
+// Задание выполнено не совсем верно, т.к. window.innerWidth и window.innerHeight отображают размеры области просмотра (окна браузера), а в задании нужно было вывести размеры экрана. Для этого используются свойства window.screen.width и window.screen.height
+// Вот хорошая статья на Хабре, где наглядно показаны различия между всеми этими размерами :)
+// https://habr.com/ru/post/509258/
